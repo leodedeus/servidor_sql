eb698db4b93a2ad699d45bff11231dbabe17c7d1
leodedeus <leorodriguesdedeus@gmail.com>
2024-12-30 13:23:09 -0300

    ajuste from end

--- app_editor/views.py
+++ app_editor/views.py
@@ -2,34 +2,6 @@ from django.shortcuts import render
 from django.db import connection
 from .models import Query
 
-'''
-def sql_editor(request):
-    result = None
-    error = None
-
-    if request.method == 'POST':
-        sql_command = request.POST.get('sql_command')  # Pega o comando enviado pelo formulário
-
-        try:
-            with connection.cursor() as cursor:
-                cursor.execute(sql_command)
-                if sql_command.strip().lower().startswith('select'):
-                    result = cursor.fetchall()  # Retorna os dados para exibir
-                else:
-                    result = f"Comando executado com sucesso: {cursor.rowcount} linhas afetadas."
-                
-            # Salva a consulta no banco de dados
-            Query.objects.create(sql_command=sql_command, result=str(result))
-
-        except Exception as e:
-            error = str(e)  # Captura qualquer erro de execução
-
-    return render(request, 'app_editor/sql_editor.html', {
-        'result': result,
-        'error': error,
-    })
-'''
-
 def sql_editor(request):
     result = None
     error = None
@@ -55,7 +27,7 @@ def sql_editor(request):
             Query.objects.create(sql_command=sql_command, result=f"Erro: {error}")
 
     # Histórico de comandos
-    query_history = Query.objects.all().order_by('-id')[:10]
+    query_history = Query.objects.all().order_by('-id')[:6]
 
     # Adicionar um indicador de sucesso ou erro para o template
     history_with_status = [
