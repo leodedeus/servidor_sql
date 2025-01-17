14f5b054f6134c0936ae7060e18aa598c54b7013
leodedeus <leorodriguesdedeus@gmail.com>
2025-01-17 17:38:20 -0300

    novas tabelas banco

--- criar_sql_tabela.py
+++ criar_sql_tabela.py
@@ -1,7 +1,7 @@
 import csv
 
 # Caminho do arquivo CSV
-csv_file = 'votacao_partidos.csv'
+csv_file = 'censo2022_demografia.csv'
 
 # Abrir o arquivo CSV com a codificação 'latin1'
 with open(csv_file, encoding='latin1') as f:
@@ -9,5 +9,5 @@ with open(csv_file, encoding='latin1') as f:
     headers = next(reader)  # Lê a primeira linha, que são os cabeçalhos
     
     # Gerar o comando SQL para criação da tabela
-    create_table_sql = f"CREATE TABLE votacao_partidos ({', '.join([f'{header} TEXT' for header in headers])});"
+    create_table_sql = f"CREATE TABLE censo2022_basico ({', '.join([f'{header} TEXT' for header in headers])});"
     print(create_table_sql)
--- verificar_csv.py
+++ verificar_csv.py
@@ -1,6 +1,6 @@
 import csv
 
-with open('votacao_partidos.csv', newline='', encoding='latin1') as csvfile:
+with open('censo2022_demografia.csv', newline='', encoding='latin1') as csvfile:
     try:
         reader = csv.reader(csvfile)
         for i, row in enumerate(reader):
